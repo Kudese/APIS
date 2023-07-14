ac3bfddee4b6ee54070c3d13e9fc662b29ae7f4f
Kudese <d.kuprichenkov@gmail.com>
2023-07-14 04:53:25 +0300

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,17 @@ function App() {
   <BrowserRouter>
 <Routes>
   <Route path="" element={<MainPage/>}>
+    <Route path="kyu8" />
+    <Route path="kyu7" />
+    <Route path="kyu6" />
+    <Route path="kyu5" />
+    <Route path="kyu4" />
+    <Route path="kyu3" />
+    <Route path="kyu2" />
+    <Route path="kyu1" />
+    <Route path="kyu0" />
+
+
   </Route>
 </Routes>
   </BrowserRouter>
